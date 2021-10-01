056d3ea140b9fea945959341b2f030a3405cee09
Canberk Akartuna <canberkakartuna@hotmail.com>
2021-10-01 22:12:39 +0300

    linear & binary search recursive way

--- search.py
+++ search.py
@@ -37,3 +37,43 @@ def findNumberByBinarySearch(numbers, numberToFind):
 
 # If the list is not sorted, then the algorithm will not work
 # Then, Time complexity of this algorithm is O(n log n) and Space complexity of this algorithm is O(1)
+
+# Linear Search Recursive:
+# Pseudo code:
+# 1. If the list is empty, return -1
+# 2. If the first element is the number, return the index
+# 3. If the first element is greater than the number, search the left half of the list
+# 4. If the first element is less than the number, search the right half of the list
+# 5. If the number is not found, return -1
+
+def findNumberByLinearSearchRecursive(numbers, numberToFind):
+    if len(numbers) == 0:
+        return -1
+    if numbers[0] == numberToFind:
+        return 0
+    return findNumberByLinearSearchRecursive(numbers[1:], numberToFind) + 1
+
+# Time complexity of this algorithm is O(n)
+# Space complexity of this algorithm is O(n)
+
+# Binary Search Recursive:
+# Pseudo code:
+# 1. If the list is empty, return -1
+# 2. Find the middle element of the list
+# 3. If the middle element is the number, return the index
+# 4. If the middle element is greater than the number, search the left half of the list
+# 5. If the middle element is less than the number, search the right half of the list
+# 6. If the number is not found, return -1
+
+def findNumberByBinarySearchRecursive(numbers, numberToFind):
+  if len(numbers) == 0:
+      return -1
+  mid = len(numbers) // 2
+  if numbers[mid] == numberToFind:
+      return mid
+  elif numbers[mid] < numberToFind:
+      return findNumberByBinarySearchRecursive(numbers[mid + 1:], numberToFind)
+  return findNumberByBinarySearchRecursive(numbers[:mid], numberToFind)
+
+# Time complexity of this algorithm is O(log n)
+# Space complexity of this algorithm is O(log n)
